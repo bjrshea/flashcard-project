461f636f750b4f3061e0fd691bffca29a3826115
Brendan Shea and Talia Haller <talia@>
2018-08-07 14:36:19 -0700

    Add entire project.

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+$(function() {
+
+  $(".clickable-1").click(function() {
+    $("#initially-hidden-1").toggle();
+    $("#initially-showing-1").toggle();
+  });
+
+  $(".clickable-2").click(function() {
+    $("#initially-hidden-2").toggle();
+    $("#initially-showing-2").toggle();
+  });
+
+  $(".clickable-3").click(function() {
+    $("#initially-hidden-3").toggle();
+    $("#initially-showing-3").toggle();
+  });
+
+  $(".clickable-4").click(function() {
+    $("#initially-hidden-4").toggle();
+    $("#initially-showing-4").toggle();
+  });
+
+  $(".clickable-5").click(function() {
+    $("#initially-hidden-5").toggle();
+    $("#initially-showing-5").toggle();
+  });
+
+  $(".clickable-6").click(function() {
+    $("#initially-hidden-6").toggle();
+    $("#initially-showing-6").toggle();
+  });
+
+  $(".clickable-7").click(function() {
+    $("#initially-hidden-7").toggle();
+    $("#initially-showing-7").toggle();
+  });
+
+  $(".clickable-8").click(function() {
+    $("#initially-hidden-8").toggle();
+    $("#initially-showing-8").toggle();
+  });
+
+  $(".clickable-9").click(function() {
+    $("#initially-hidden-9").toggle();
+    $("#initially-showing-9").toggle();
+  });
+
+});
